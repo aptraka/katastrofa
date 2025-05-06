70430795934e6ddedd97e7ade9626755bac8c0e0
Aditya Putra Kejora <38183313+aptraka@users.noreply.github.com>
2025-05-06 14:29:55 +0700

    Update config.js Update BASE URL

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    BASE_URL: 'https://apikatastrofa.pramuka1133.cloud/',
+    BASE_URL: 'https://apikatastrofa.pramuka1133.my.id/',
     DEFAULT_LANGUAGE: 'en-us',
     CACHE_NAME: new Date().toISOString(),
     BASE_WEATHER: 'https://api.weatherapi.com/v1/current.json?key=a97003e04a4644baa5313346240306&q=Jakarta&aqi=yes',
